17aa6e0729d23986b3b2d36c1ff632c7bafdaa2a
JaeWon <hy87412@naver.com>
2024-09-11 15:25:36 +0900

    9/11 나이스 api에서 안당겨지는건지 내 서버가 json못보내는지 근데 test메소드는 잘됨

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ requestform = {'KEY' : '322f1dd4d7da4766a9c6828c4d861880',
                'Type' : 'json',
                'plndex' : 1,
                'pSize' : 100}
-niceurl = 'https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=322f1dd4d7da4766a9c6828c4d861880&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8750594'
+niceurl = 'https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=322f1dd4d7da4766a9c6828c4d861880&MLSV_YMD=20240911&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8750594'
 @app.route('/')
 def lobby():
     return 'on running'
+
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify(requestform)
 
 @app.route('/request', methods=['GET'])
 def sendData():
 
     respone = request.get_json(niceurl)
 
-    return respone
+    return jsonify(respone)
 
 
 if __name__ == "__main__":
