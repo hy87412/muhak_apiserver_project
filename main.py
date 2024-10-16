03ab166d0214c462b12450f6fb441d190385acbf
JaeWon <hy87412@naver.com>
2024-10-16 15:16:11 +0900

    beta page

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import datetime
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import requests
 import json
 app = Flask(__name__)
 
-requestform = {'KEY':'322f1dd4d7da4766a9c6828c4d861880',
-               'Type':'json',
-               'plndex':1,
-               'pSize':100}
+# requestform = {'KEY':'322f1dd4d7da4766a9c6828c4d861880',
+#                'Type':'json',
+#                'plndex':1,
+#                'pSize':100}
 basicform1 = 'https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=322f1dd4d7da4766a9c6828c4d861880&MLSV_YMD='
 basicform2 = '&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8750594'
+niceurl = 'https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=322f1dd4d7da4766a9c6828c4d861880&MLSV_YMD=20241016&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8750594'
+
+
 def getdate() :
     time = datetime.datetime.now()
     time = datetime.datetime.now()
@@ -19,24 +22,20 @@ def getdate() :
     todaydate = year + month + day
     result = str(todaydate)
     return result
-niceurl = 'https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=322f1dd4d7da4766a9c6828c4d861880&MLSV_YMD=20241016&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=R10&SD_SCHUL_CODE=8750594'
 
-
-
-@app.route('/')
+@app.route('/state')
 def lobby():
     return 'on running'
 
-@app.route('/test', methods=['GET'])
-def test():
-    return jsonify(requestform)
-
 @app.route('/request', methods=['GET'])
 def sendData():
-
     respone = requests.post(basicform1 + getdate() + basicform2)
     data = respone.json()
     return data
+
+@app.route('/')
+def page():
+    return render_template('bobpage.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
