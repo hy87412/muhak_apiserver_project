4a663936511efeed6cf912271766380d4ff5ae2d
JaeWon <hy87412@naver.com>
2024-11-09 10:58:53 +0900

    bobpage color change #354550

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ originpageform = """<!DOCTYPE html>
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
-            background-color: #f9f9f9;
+            background-color: #354550;
             display: flex;
             flex-direction: column;
             align-items: center;
